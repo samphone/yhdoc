78bd93554aaedfcd2cc1e6d07689b64d84e4b436
代赛峰 <daisaifeng@daisaifengdeMacBook-Pro.local>
2021-10-14 16:03:17 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,51 @@
+const moment = require('moment');
+
+module.exports = {
+    base: '/',
+    title: '英行前端项目文档',
+    description: '英行前端项目文档',
+    themeConfig: {
+        logo: '/logo.png',
+        nav: [
+            {
+                text: 'github',
+                link: '/b'
+            },
+        ],
+        sidebar: [
+            {
+                title: '背景',
+                collapsable: false,
+                children: [
+                    'background/1-起源',
+                    'background/2-项目迭代历史',
+                    'background/3-常见对接注意点'
+                ]
+            },
+            {
+                title: 'H5-机票',
+                collapsable: false,
+                children: [
+                    'h5Flight/1-简介',
+                    'h5Flight/2-目录介绍',
+                    'h5Flight/3-主要逻辑分析',
+                    'h5Flight/4-其他',
+                ]
+            }
+        ],
+        lastUpdated: 'Last Updated',
+    },
+    plugins: [
+        [
+            '@vuepress/last-updated',
+            {
+            transformer: (timestamp, lang) => {
+                // 不要忘了安装 moment
+                const moment = require('moment')
+                moment.locale(lang)
+                return moment(timestamp).fromNow()
+            }
+            }
+        ]
+    ]
+}
