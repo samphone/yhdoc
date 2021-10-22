3896167306ffc0fefdff605e5ad7f3190820879c
代赛峰 <daisaifeng@daisaifengdeMacBook-Pro.local>
2021-10-22 18:19:57 +0800

    列表页面

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,8 +14,8 @@ module.exports = {
         ],
         sidebar: [
             {
-                title: '背景',
-                collapsable: false,
+                title: 'background',
+                collapsable: true,
                 children: [
                     'background/1-起源',
                     'background/2-常见对接注意点',
@@ -23,14 +23,69 @@ module.exports = {
                 ]
             },
             {
-                title: 'H5-机票',
-                collapsable: false,
+                title: 'H5-flight',
+                collapsable: true,
                 sidebarDepth: 2,
                 children: [
                     'h5Flight/1-简介',
                     'h5Flight/2-目录介绍',
                     'h5Flight/3-主要逻辑分析',
                     'h5Flight/4-其他',
+                ]
+            },
+            {
+                title: 'H5-hotel',
+                collapsable: true,
+                sidebarDepth: 2,
+                children: [
+                    'h5Hotel/1-简介',
+                    'h5Hotel/2-目录介绍',
+                    'h5Hotel/3-主要逻辑分析',
+                    'h5Hotel/4-其他',
+                ]
+            },
+            {
+                title: 'H5-train',
+                collapsable: true,
+                sidebarDepth: 2,
+                children: [
+                    'h5Train/1-简介',
+                    'h5Train/2-目录介绍',
+                    'h5Train/3-主要逻辑分析',
+                    'h5Train/4-其他',
+                ]
+            },
+            {
+                title: 'H5-common',
+                collapsable: true,
+                sidebarDepth: 2,
+                children: [
+                    'h5Common/1-简介',
+                    'h5Common/2-目录介绍',
+                    'h5Common/3-主要逻辑分析',
+                    'h5Common/4-其他',
+                ]
+            },
+            {
+                title: 'H5-blindBox',
+                collapsable: true,
+                sidebarDepth: 2,
+                children: [
+                    'h5BlindBox/1-简介',
+                    'h5BlindBox/2-目录介绍',
+                    'h5BlindBox/3-主要逻辑分析',
+                    'h5BlindBox/4-其他',
+                ]
+            },
+            {
+                title: 'App',
+                collapsable: true,
+                sidebarDepth: 2,
+                children: [
+                    'yhApp/1-简介',
+                    'yhApp/2-目录介绍',
+                    'yhApp/3-主要逻辑分析',
+                    'yhApp/4-其他',
                 ]
             }
         ],
